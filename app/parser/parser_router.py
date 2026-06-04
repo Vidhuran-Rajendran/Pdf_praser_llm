bc3f582e8f39a56d3da0e31f9c4f18492178f470
Vidhuran-Rajendran <vidhu.vidhuran@gmail.com>
2026-06-04 10:55:10 +0530

    new

--- app/parser/parser_router.py
+++ app/parser/parser_router.py
@@ -1,9 +1,10 @@
 from app.config import MODE
-from app.parser.unstructured_parser import parse_unstructured
+from app.parser.pymupdf_parser import parse_pymupdf
 from app.parser.gcp_parser import parse_gcp
+
 
 def parse_document(file_path):
     if MODE == "paid":
         return parse_gcp(file_path)
 
-    return parse_unstructured(file_path)
+    return parse_pymupdf(file_path)
